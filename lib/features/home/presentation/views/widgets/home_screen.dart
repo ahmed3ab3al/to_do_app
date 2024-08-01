c3fc21c6bdf906fa1f38efe2f3ce50ab46347b35
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2024-08-02 00:16:14 +0300

    Handel BottomSheet

--- lib/features/home/presentation/views/widgets/home_screen.dart
+++ lib/features/home/presentation/views/widgets/home_screen.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:to_do_app/core/widgets/custom_text_form_field.dart';
 import 'package:to_do_app/features/archived_tasks/presentation/views/widgets/archived_task_screen.dart';
 import 'package:to_do_app/features/done_tasks/presentation/views/widgets/done_task_screen.dart';
 import 'package:to_do_app/features/new_tasks/presentation/views/widgets/new_task_screen.dart';
@@ -17,9 +19,13 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int currentIndex = 0;
+  TextEditingController titleController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+  TextEditingController timeController = TextEditingController();
   bool isBottomSheetShown = false;
   IconData fabIcon = Icons.edit;
   var scaffoldKey = GlobalKey<ScaffoldState>();
+  var formKey = GlobalKey<FormState>();
   @override
   void initState() {
     createDatabase();
@@ -50,24 +56,84 @@ class _HomeScreenState extends State<HomeScreen> {
           backgroundColor: Colors.blue,
           onPressed: () {
             if (isBottomSheetShown) {
-              Navigator.of(context).pop();
-              isBottomSheetShown = false;
-              setState(() {
-                fabIcon = Icons.edit;
-              });
+              if (formKey.currentState!.validate()) {
+                insertToDatabase(
+                    title: titleController.text,
+                    date: dateController.text,
+                    time: timeController.text)
+                    .then((value) {
+                  Navigator.of(context).pop();
+                  isBottomSheetShown = false;
+                  setState(() {
+                    fabIcon = Icons.edit;
+                  });
+                });
+              }
+
             } else {
-              scaffoldKey.currentState!.showBottomSheet((context) => Container(
-                    width: double.infinity,
-                    height: 50,
-                    color: Colors.blue,
-                  ));
+              scaffoldKey.currentState!.showBottomSheet((context) =>
+                  Container(
+                    color: Colors.white.withOpacity(.001),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Form(
+                        key: formKey,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            CustomTextFormFiled(
+                                customController: titleController,
+                                type: TextInputType.text,
+                                label: ' Task Title',
+                                prefix: Icons.title),
+                            const SizedBox(
+                              height: 15,
+                            ),
+                            CustomTextFormFiled(
+                                customController: timeController,
+                                type: TextInputType.datetime,
+                                label: ' Task Time',
+                                prefix: Icons.watch_later_outlined,
+                              onTap: () {
+                                  showTimePicker(context: context,
+                                      initialTime: TimeOfDay.now())
+                                      .then((value) {
+                                        timeController.text = value!.format(context).toString();
+                                      });
+                              }
+                            ),
+                            const SizedBox(
+                              height: 15,
+                            ),
+                            CustomTextFormFiled(
+                                customController: dateController,
+                                type: TextInputType.datetime,
+                                label: ' Task Date',
+                                prefix: Icons.calendar_today,
+                              onTap: () {
+                                  showDatePicker(context: context,
+                                      initialDate: DateTime.now(),
+                                      firstDate: DateTime.now(),
+                                      lastDate: DateTime.parse('2025-12-31'))
+                                      .then((value) {
+                                        dateController.text =DateFormat.yMMMd().format(value!);
+                                      });
+                              }
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                elevation: 30
+              );
               isBottomSheetShown = true;
               setState(() {
                 fabIcon = Icons.add;
               });
             }
           },
-          child: Icon(fabIcon),
+          child: Icon(fabIcon, color: Colors.white),
         ),
         bottomNavigationBar: BottomNavigationBar(
           type: BottomNavigationBarType.fixed,
@@ -112,11 +178,11 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  void insertToDatabase() async {
-    await database.transaction((txn) async {
+  Future insertToDatabase({required String title, required String date, required String time}) async {
+    return await database.transaction((txn) async {
       txn
           .rawInsert(
-              'INSERT INTO ToDo (title,date,time,status) VALUES ("new task","2022-12-12","12:12:12","new")')
+              'INSERT INTO ToDo (title,date,time,status) VALUES ("$title","$date","$time","new")')
           .then((value) {
         print('$value inserted successfully');
       }).catchError((onError) {});
